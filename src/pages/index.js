09993cded3ea332bfdfd0ced1e0f44a4fb7f25f4
Mathew <mathewhleland@gmail.com>
2019-05-04 15:51:06 -0700

    added styled-components 💅🏻

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,11 +5,33 @@ import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
 
+import styled from "styled-components"
+
+const Button = styled.button`
+  background: ${props => (props.safe ? "green" : "red")};
+  height: ${props => (props.huge ? "400px" : "100px")};
+  width: 200px;
+  color: blue;
+`
+
+const buttonStyle = {
+  background: "yellow",
+}
+
 const IndexPage = () => (
   <Layout>
-    <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
+    <SEO title="Home" keywords={[`found`, `lost`, `kitten`]} />
     <h1>Hi kitten friends</h1>
     <p>Welcome to your new Gatsby site.</p>
+    <Button safe={true}> Safe</Button>
+    <Button safe> Unsafe</Button>
+    <Button>more</Button>
+    <Button>more</Button>
+    <Button>more</Button>
+    <Button>more</Button>
+    <Button>more</Button>
+    <Button>more</Button>
+
     <p>Now go build something great.</p>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
